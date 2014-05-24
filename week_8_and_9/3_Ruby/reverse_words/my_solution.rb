cb1bd21c45c7976377ee792abd9ecc33444fd176
absarora <absingharora@gmail.com>
2014-05-23 17:00:43 -0700

    Update my_solution.rb

--- week_8_and_9/3_Ruby/reverse_words/my_solution.rb
+++ week_8_and_9/3_Ruby/reverse_words/my_solution.rb
@@ -1,29 +1,32 @@
 # U3.W8-9: Reverse Words
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # 2. Pseudocode
 
+# define a method named reverse_words that takes a string as an argument
+# convert the string to an array and split each word using a whitespace
+# reverse each word
+# convert an array to a string (space every word)
 
 
 # 3. Initial Solution
 
-
-
+def reverse_words(str)
+  str.split(" ").map{ |i| i.reverse }.join(" ")
+end
 
 # 4. Refactored Solution
 
-
-
-
-
+def reverse_words(str)
+  str.split(" ").map(&:reverse).join(" ")
+end
 
 # 1. DRIVER TESTS/ASSERT STATEMENTS GO BELOW THIS LINE
 
+p reverse_words("Devbootcamp should invest in a coding house")
 
+# 5. Reflection
 
-
-
-
-# 5. Reflection 
+# This was a fairly easy exercise to work on.
