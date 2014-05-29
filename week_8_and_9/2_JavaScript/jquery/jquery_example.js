1dcbe384a4e40eb841bd224c0650421f8ee22b7b
absarora <absingharora@gmail.com>
2014-05-29 00:15:30 -0700

    Update jquery_example.js

--- week_8_and_9/2_JavaScript/jquery/jquery_example.js
+++ week_8_and_9/2_JavaScript/jquery/jquery_example.js
@@ -6,22 +6,38 @@ $(document).ready(function(){
 $('body').css({'background-color': 'pink'})
  
 //RELEASE 1:
-  //Add code here to select elements of the DOM 
+   var bodyElement = $('body')
  
  
 //RELEASE 2: 
   // Add code here to modify the css and html of DOM elements
- 
+ $('h1').css({
+  'color': 'blue',
+  'border': '2px solid',
+  'visibility': 'visible'
+  });
+   $('div.mascot > h1').html("Coyotes")
  
 //RELEASE 3: Event Listener
   // Add the code for the event listener here 
- 
+ $('img').on('mouseover', function(e){
+     e.preventDefault()
+    $(this).attr('src', 'https://bitbucket-assetroot.s3.amazonaws.com/c/photos/2013/Dec/10/pauleyj-avatar-496208782-4_avatar.png')
+  });
+
+ $('img').on('mouseleave', function(e){
+    e.preventDefault();
+    $(this).attr('src', 'http://photos2.meetupstatic.com/photos/sponsor/d/0/b/8/iab120x90_1853432.jpeg');
+  });
  
 //RELEASE 4 : Experiment on your own
  
- 
- 
- 
- 
+ $('img').click(function(){
+    $(this).animate({
+      width: '100px',
+      height: '100px',
+      boder: 'solid'
+    }, 1000);
+  });
  
 })  // end of the document.ready function: do not remove or write DOM manipulation below this.
